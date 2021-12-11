005defe184fd5ac402da4908729e8c851474485a
flavioxaviersilva <56923158+flavioxaviersilva@users.noreply.github.com>
2021-12-10 22:31:59 -0300

    Delete lock-n-roll directory

--- lock-n-roll/src/actions/index.js
+++
@@ -1,6 +0,0 @@
-//pego todos os nomes exportes e estou fazendo como se tivesse sendo exportado do index do actions
-
-export * from './userActions';
-export * from './atividadeFormActions';
-export * from './firebaseActions';
-export * from './userGoogleActions';
